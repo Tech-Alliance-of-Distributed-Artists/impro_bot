36431dbf1c0704234f97c3a6b2c59509d7cfc39f
Ian Carroll <ian.d.carroll@gmail.com>
2019-11-08 17:51:43 -0800

    Define configuration options

--- bot.js
+++ bot.js
@@ -1,1 +1,11 @@
 const tmi = require('tmi.js');
+
+const opts = {
+  identity: {
+    username: process.env.BOT_USERNAME,
+    password: process.env.OAUTH_TOKEN
+  },
+  channels: [
+    process.env.CHANNEL_NAME
+  ]
+};
