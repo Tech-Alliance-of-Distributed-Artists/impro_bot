5e7e8f78e3b056cf24384c9fcf479b3cc539e2c2
Ian Carroll <ian.d.carroll@gmail.com>
2019-11-13 13:41:49 -0800

    Abstract onMessageReceived; refactor logging

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
-const log = require('./lib/report/toConsole');
-const onConnected = require('./lib/handle/connection');
+const tellMeImConnected = require('./lib/report/toConsole');
+const onMessageReceived = require('./lib/handle/message');
 const tmi = require('tmi.js');
 
 const opts = {
@@ -14,46 +14,14 @@ const opts = {
 
 const client = new tmi.client(opts);
 
-client.on('connected', (addr, port) => { log(onConnected(addr, port)); });
-client.on('message', onMessageHandler);
+client.on('connected', tellMeImConnected);
+client.on('message', respondToGood);
 
 client.connect();
 
-function onMessageHandler (target, context, msg, self) {
-  if (doNothing(self)) { return; }
-  const command = parseMessage(msg);
-  const response = buildResponse(command);
-  if (response !== DO_NOT_REPLY) { send(response, target); }
+function respondToGood(target, context, msg, self) {
+  const response = onMessageReceived(target, context, msg, self);
+  if (sendable(response)) { client.say(target, response); }
 }
 
-function doNothing(self) { return !!self; }
-
-function parseMessage(msg) { return msg.trim(); }
-
-function buildResponse(command) {
-  if (known(command)) {
-    return handleKnown(command);
-  }
-  return handleUnknown(command);
-}
-
-function send(response, target) {
-  client.say(target, response);
-}
-
-function known(command) {
-  const commands = ['suggest'];
-  return command === commands[0];
-}
-
-function handleKnown(command) {
-  console.log(`* That's a ${command} command!`);
-  return `You made a ${command} command! You're a good Charlie-compound Boy! Link: https://improtheatre.com`
-}
-
-const DO_NOT_REPLY = `RESPONSE NOT NECESSARY`;
-
-function handleUnknown(command) {
-  console.log(`* I don't know what ${command} means!`);
-  return DO_NOT_REPLY;
-}
+function sendable(response) { return !!response; }
---
+++ spec/handle/messageSpec.js
@@ -0,0 +1,5 @@
+describe("message", function() {
+  it("is a well configured spec", function() {
+    expect(true).toEqual(true);
+  });
+});
