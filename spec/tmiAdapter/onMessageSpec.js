940af1dbdea313ac44b48daae4f1a9734c4ef0b6
Ian Carroll <ian.d.carroll@gmail.com>
2019-11-15 16:30:47 -0800

    Test message adapter

---
+++ spec/tmiAdapter/onMessageSpec.js
@@ -0,0 +1,35 @@
+const subject = require('../../lib/tmiAdapter/onMessage');
+
+describe("tmiAdapter/onMessage/respondToGood", function() {
+  it("passes a response to a callback from a valid message", function () {
+    const target = "";
+    const context = "";
+    const message = "suggest";
+    const self = false;
+
+    const spy = new Spy();
+    subject(target, context, message, self, spy.recordWhatIsSent);
+
+    expect(spy.actual()).toContain(message);
+  });
+
+  it("does not pass a message that needs not be responded to", function () {
+    const target = "";
+    const context = "";
+    const message = "No reply necessary, but know that I love you.";
+    const self = false;
+
+    const spy = new Spy();
+    subject(target, context, message, self, spy.recordWhatIsSent);
+
+    expect(spy.actual()).toContain("I have not yet been called");
+  });
+});
+
+function Spy() { Spy.prototype.response = "I have not yet been called"; }
+Spy.prototype.recordWhatIsSent = function(target, response) {
+  Spy.prototype.response = response;
+};
+Spy.prototype.actual = function() {
+  return Spy.prototype.response;
+};
