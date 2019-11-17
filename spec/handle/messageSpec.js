94bc83a9365b37167e2b6f133d107d5060c484b5
Ian Carroll <ian.d.carroll@gmail.com>
2019-11-17 12:59:43 -0800

    message/buildResponse() always returns a string, never undefined

--- spec/handle/messageSpec.js
+++ spec/handle/messageSpec.js
@@ -1,5 +1,36 @@
-describe("message", function() {
-  it("is a well configured spec", function() {
-    expect(true).toEqual(true);
+const subject = require('../../lib/handle/message');
+
+describe("handle/message/buildResponse", function() {
+  it("builds a response string from a valid message", function () {
+    const target = "";
+    const context = "";
+    const message = "suggest";
+    const self = false;
+
+    const actual = subject(target, context, message, self);
+
+    expect(actual).toContain(message);
+  });
+
+  it("builds a DO_NOT_REPLY message for messages that need no response", function () {
+    const target = "";
+    const context = "";
+    const message = "No reply necessary, but know that I love you.";
+    const self = false;
+
+    const actual = subject(target, context, message, self);
+
+    expect(actual).toEqual("");
+  });
+
+  it("does not respond to itself even if the message is valid", function () {
+    const target = "";
+    const context = "";
+    const message = "suggest";
+    const self = true;
+
+    const actual = subject(target, context, message, self);
+
+    expect(actual).toEqual("");
   });
 });
