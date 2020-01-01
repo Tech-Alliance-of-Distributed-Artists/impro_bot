4dbdd2d9207915f3d9e66b9ef8ab060ee3ebd180
Ian Carroll <ian.d.carroll@gmail.com>
2020-01-01 13:30:19 -0800

    Add specs to test route message directly

---
+++ spec/handle/route/messageSpec.js
@@ -0,0 +1,29 @@
+const constants = require('../../../lib/signal/constants');
+const DO_NOT_REPLY = constants.DO_NOT_REPLY;
+const SUGGESTION = constants.SUGGESTION;
+const subject = require('../../../lib/handle/route/message');
+
+describe("handle/message/buildResponse", function() {
+  it("returns a message containing the SUGGEST command when given the SUGGEST command", function () {
+    const command = SUGGESTION;
+    const actual = subject(command);
+    expect(actual).toContain(command);
+  });
+
+  it("returns a DO_NOT_REPLY message when given a DO_NOT_REPLY command", function () {
+    const command = "pet the wallaby";
+    const actual = subject(command);
+    expect(actual).toEqual(DO_NOT_REPLY);
+  });
+
+  it("returns a DO_NOT_REPLY message for commands it does not know", function () {
+    const command = "pet the wallaby";
+    const actual = subject(command);
+    expect(actual).toEqual(DO_NOT_REPLY);
+  });
+
+  it("returns a DO_NOT_REPLY message when given no command at all", function () {
+    const actual = subject();
+    expect(actual).toEqual(DO_NOT_REPLY);
+  });
+});
