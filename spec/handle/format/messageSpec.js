f8885f2930a42669efaa44ccd312a3a3c533f572
Ian Carroll <ian.d.carroll@gmail.com>
2020-01-01 13:16:10 -0800

    Add specs to test format message directly

---
+++ spec/handle/format/messageSpec.js
@@ -0,0 +1,12 @@
+const subject = require('../../../lib/handle/format/message');
+
+describe("handle/message/format/formatReply", function() {
+  it("builds a response string from the command passed to it", function () {
+    const command = "Farmboy, fetch me that pitcher";
+    const reply = `You made a ${command} command! You're a good Charlie-compound Boy! Link: https://improtheatre.com`
+
+    const actual = subject(command);
+
+    expect(actual).toEqual(reply);
+  });
+});
